e96c7ed0a3d8631ec0007af422db3669e7a0398b
ethan-stone <ethan.stone9352@gmail.com>
2025-01-05 22:51:14 -0500

    day 2 part 2

--- advent_of_code_2015_rust/src/day_2.rs
+++ advent_of_code_2015_rust/src/day_2.rs
@@ -29,11 +29,11 @@ impl PresentBox {
     }
 
     pub fn smallest_side_area(&self) -> u32 {
-        let s1 = self.length * self.width;
-        let s2 = self.width * self.height;
-        let s3 = self.height * self.length;
+        let a1 = self.length * self.width;
+        let a2 = self.width * self.height;
+        let a3 = self.height * self.length;
 
-        let min_option = vec![s1, s2, s3].iter().min().copied();
+        let min_option = vec![a1, a2, a3].iter().min().copied();
 
         let min = match min_option {
             Some(val) => val,
@@ -46,6 +46,29 @@ impl PresentBox {
     pub fn total_required_wrapping_paper(&self) -> u32 {
         self.total_surface_area() + self.smallest_side_area()
     }
+
+    pub fn smallest_side_perimeter(&self) -> u32 {
+        let p1 = 2 * self.length + 2 * self.width;
+        let p2 = 2 * self.width + 2 * self.height;
+        let p3 = 2 * self.height + 2 * self.length;
+
+        let min_option = vec![p1, p2, p3].iter().min().copied();
+
+        let min = match min_option {
+            Some(val) => val,
+            None => panic!("Somehow vector is empty!"),
+        };
+
+        min
+    }
+
+    pub fn volume(&self) -> u32 {
+        self.length * self.width * self.height
+    }
+
+    pub fn total_required_ribbon(&self) -> u32 {
+        self.smallest_side_perimeter() + self.volume()
+    }
 }
 
 pub fn day_2() -> Result<(), Box<dyn Error>> {
@@ -53,15 +76,19 @@ pub fn day_2() -> Result<(), Box<dyn Error>> {
 
     let mut total_wrapping_paper_needed = 0;
 
+    let mut total_ribbon_needed = 0;
+
     for dimensions in input.lines() {
         let present_box = PresentBox::build(dimensions)?;
 
         total_wrapping_paper_needed += present_box.total_required_wrapping_paper();
+        total_ribbon_needed += present_box.total_required_ribbon()
     }
 
     println!(
         "Day 2: Elves need a total of {total_wrapping_paper_needed} square feet of wrapping paper"
     );
+    println!("Day 2: Elves need a total of {total_ribbon_needed} feed of ribbon");
 
     Ok(())
 }
