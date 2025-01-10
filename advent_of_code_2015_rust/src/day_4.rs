1edd559147845955c36e67c6d396400ecaf5b76d
ethan-stone <ethan.stone9352@gmail.com>
2025-01-10 13:12:40 -0500

    day 4 part 1

---
+++ advent_of_code_2015_rust/src/day_4.rs
@@ -0,0 +1,26 @@
+use hex;
+use md5::{Digest, Md5};
+
+pub fn day_4() {
+    let input = "yzbqklnj";
+
+    let mut start = 0;
+
+    loop {
+        let mut hasher = Md5::new();
+
+        hasher.update(format!("{input}{start}"));
+
+        let result = hasher.finalize();
+
+        let result_str = hex::encode(result);
+
+        if result_str.starts_with("00000") {
+            break;
+        }
+
+        start += 1;
+    }
+
+    println!("Day 4: The number is {start}");
+}
--- advent_of_code_2015_rust/src/main.rs
+++ advent_of_code_2015_rust/src/main.rs
@@ -3,11 +3,13 @@ use std::error::Error;
 mod day_1;
 mod day_2;
 mod day_3;
+mod day_4;
 
 fn main() -> Result<(), Box<dyn Error>> {
     day_1::day_1();
     day_2::day_2()?;
     day_3::day_3()?;
+    day_4::day_4();
 
     Ok(())
 }
